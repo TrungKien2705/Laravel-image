1789746e1256799cada12b50bcfc4f572f7b31ca
TrungKien2705 <nt.kien.2705@gmail.com>
2023-08-25 16:50:41 +0700

    first commit

---
+++ database/migrations/2023_08_24_045420_create_image_manipulations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Album;
+
+class CreateImageManipulationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('image_manipulations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('path', 2000);
+            $table->string('type', 255);
+            $table->text('data');
+            $table->string('output_path', 2000)->nullable();
+            $table->timestamp('create_at')->nullable();
+            $table->foreignIdFor(User::class, 'user_id')->nullable();
+            $table->foreignIdFor(Album::class, 'album_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('image_manipulations');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\V1\AlbumController;
+use \App\Http\Controllers\V1\ImageManipulationController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('v1')->group(function () {
+        Route::apiResource('album', AlbumController::class);
+        Route::get('images', [ImageManipulationController::class, 'index']);
+        Route::get('images/by-album/{album}', [ImageManipulationController::class, 'byAlbum']);
+        Route::get('images/{images}', [ImageManipulationController::class, 'show']);
+        Route::post('images/resize', [ImageManipulationController::class, 'resize']);
+        Route::delete('images', [ImageManipulationController::class, 'destroy']);
+    });
+});
+
